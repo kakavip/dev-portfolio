65ddc99193337e829f72eee730e73d8d4524a10b
quangnd <quang.ngo@youthdev.net>
2019-11-23 15:26:12 +0700

    Init micorfrontend for my portofilo.

---
+++ single-spa.config.js
@@ -0,0 +1,11 @@
+import {start, registerApplication} from 'single-spa';
+
+// registerApplication(
+//     'home',
+//     () => {},
+//     (location) => location.pathname === ""||
+//         location.pathname ==="/" ||
+//         location.pathname === "/home"
+// );
+
+start();
